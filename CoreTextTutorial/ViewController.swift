0cb4a9839e453b9772b0dc9739617911e4042619
bathant hegazy <bathanthegazy94@gmail.com>
2020-12-15 12:32:15 +0200

    first commit

---
+++ CoreTextTutorial/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CoreTextTutorial
+//
+//  Created by Bathant on 15/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
